dd477d66708736fa126b70574bcebda90f510956
Tahir <moonmustafvi92@gmail.com>
2024-05-06 23:00:07 +0500

    Two-Sum Problem

--- 1.py
+++ 1.py
@@ -1,14 +1,28 @@
-# Two-Sum returns the two indexes that sum up to a target!
+# Two-Sum returns the two indexes that sum up to a target, uses dictionary to do in O(n)!
+##### Brute Force ######
+# for i in range(len(nums)):
+#     for j in range(i + 1, len(nums)):
+#         if nums[i] + nums[j] == target:
+#             return [i, j]
+##### Brute Force ######
+
 
 from typing import List
 
 
 class Solution:
+    # Subtracts number from the target and looks for it in dictionary,
+    # if found okay, else add the number to dictionary and look for next one
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        lookup = {}
         for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+            comp = target - nums[i]
+            if comp in lookup:
+                return [lookup[comp], i]
+            else:
+                lookup[nums[i]] = i
+
+        return []
 
 
 def main():
